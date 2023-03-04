f99442bcd4edff9f388d5a61790ede69cf2ed500
Jaey <jaeyoung.kim@student.manchester.ac.uk>
2023-03-04 17:25:19 +0000

    BOJ1008

---
+++ javaboj/java1008/boj1008.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class boj1008 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    double A = sc.nextInt();
+    double B = sc.nextInt();
+    System.out.println(A / B);
+    sc.close();
+  }
+}
